255281ac89c494f5c95c090b11a64a45ca1e5ede
Markus Sabadello <markus@danubetech.com>
2018-03-14 16:04:18 +0100

    Chrome compatibility.

--- didauth.js
+++ didauth.js
@@ -1,10 +1,12 @@
+var api = typeof browser == 'object' ? browser : chrome;
+
 window.addEventListener("message", function(event) {
 
 	if (event.source != window) return;
 
 	if (event.data.type && (event.data.type == "DID_AUTH")) {
 
-		browser.storage.local.get(['dids', 'didnr', 'vcs', 'vc1', 'vc2']).then(function(value) {
+		api.storage.local.get(['dids', 'didnr', 'vcs', 'vc1', 'vc2']).then(function(value) {
 
 			var text = "";
 			text += "Submit DID Auth Verifiable Credential for <strong>" + Object.keys(value.dids)[value.didnr] + "</strong>. ";
@@ -36,16 +38,16 @@ window.addEventListener("message", function(event) {
 	}
 }, false);
 
-browser.storage.local.get(['dids', 'didnr', 'vc1', 'vc2']).then(function(value) {
+api.storage.local.get(['dids', 'didnr', 'vc1', 'vc2']).then(function(value) {
 
 	var dids = {'did:sov:DavnUKB3kjn7VmVZXzEDL7': {"type": ["Credential", "DidAuthCredential"],"issuer": "did:sov:DavnUKB3kjn7VmVZXzEDL7","issued": "2010-01-01","claim": {"id": "did:sov:DavnUKB3kjn7VmVZXzEDL7","publicKey": "did:sov:DavnUKB3kjn7VmVZXzEDL7#keys-1"},"proof": {"type": "Secp256k1Signature2018","creator": "did:sov:DavnUKB3kjn7VmVZXzEDL7#keys-1","created": "2018-01-01T21:19:10Z","nonce": "...","signatureValue": "..."}},
 		'did:btcr:xkrn-xzcr-qqlv-j6sl': {"type": ["Credential", "DidAuthCredential"],"issuer": "did:btcr:xkrn-xzcr-qqlv-j6sl","issued": "2010-01-01","claim": {"id": "did:btcr:xkrn-xzcr-qqlv-j6sl","publicKey": "did:btcr:xkrn-xzcr-qqlv-j6sl#keys-1"},"proof": {"type": "Secp256k1Signature2018","creator": "did:btcr:xkrn-xzcr-qqlv-j6sl#keys-1","created": "2018-01-01T21:19:10Z","nonce": "...","signatureValue": "..."}},
 		'did:v1:test:nym:3AEJTDMSxDDQpyUftjuoeZ2Bazp4Bswj1ce7FJGybCUu': {"type": ["Credential", "DidAuthCredential"],"issuer": "did:v1:test:nym:3AEJTDMSxDDQpyUftjuoeZ2Bazp4Bswj1ce7FJGybCUu","issued": "2010-01-01","claim": {"id": "did:v1:test:nym:3AEJTDMSxDDQpyUftjuoeZ2Bazp4Bswj1ce7FJGybCUu","publicKey": "did:v1:test:nym:3AEJTDMSxDDQpyUftjuoeZ2Bazp4Bswj1ce7FJGybCUu#keys-1"},"proof": {"type": "Secp256k1Signature2018","creator": "did:v1:test:nym:3AEJTDMSxDDQpyUftjuoeZ2Bazp4Bswj1ce7FJGybCUu#keys-1","created": "2018-01-01T21:19:10Z","nonce": "...","signatureValue": "..."}}};
 	var vcs = [{	"type": ["Credential"],	"issuer": "did:sov:0000000000",	"issued": "2010-01-01",	"claim": {		"id": "did:sov:1234567890",		"management": "did:sov:DavnUKB3kjn7VmVZXzEDL7"	},	"proof": {		"type": "Ed25519Signature2018",		"created": "2018-01-01T21:19:10Z",		"creator": "did:sov:0000000000#keys-1",		"nonce": "...",		"signatureValue": "..."	}},
 		{	"type": ["Credential"],	"issuer": "did:sov:DavnUKB3kjn7VmVZXzEDL7",	"issued": "2010-01-01",	"claim": {		"id": "did:sov:1234567890",		"management": "did:v1:test:nym:3AEJTDMSxDDQpyUftjuoeZ2Bazp4Bswj1ce7FJGybCUu"	},	"proof": {		"type": "Ed25519Signature2018",		"created": "2018-01-01T21:19:10Z",		"creator": "did:sov:DavnUKB3kjn7VmVZXzEDL7#keys-1",		"nonce": "c0ae1c8e-c7e7-469f-b252-86e6a0e7387e",		"signatureValue": "..."	}}];
-	if (! value.dids) browser.storage.local.set({ 'dids': dids });
-	if (! value.didnr) browser.storage.local.set({ 'didnr': 0 });
-	if (! value.vcs) browser.storage.local.set({ 'vcs': vcs });
-	if (! value.vc1) browser.storage.local.set({ 'vc1': false });
-	if (! value.vc2) browser.storage.local.set({ 'vc2': false });
+	if (! value.dids) api.storage.local.set({ 'dids': dids });
+	if (! value.didnr) api.storage.local.set({ 'didnr': 0 });
+	if (! value.vcs) api.storage.local.set({ 'vcs': vcs });
+	if (! value.vc1) api.storage.local.set({ 'vc1': false });
+	if (! value.vc2) api.storage.local.set({ 'vc2': false });
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,28 +1,30 @@
+var api = typeof browser == 'object' ? browser : chrome;
+
 function onRadioClick1() {
 
-	browser.storage.local.set({ 'didnr': 0 });
+	api.storage.local.set({ 'didnr': 0 });
 }
 
 function onRadioClick2() {
 
-	browser.storage.local.set({ 'didnr': 1 });
+	api.storage.local.set({ 'didnr': 1 });
 }
 
 function onRadioClick3() {
 
-	browser.storage.local.set({ 'didnr': 2 });
+	api.storage.local.set({ 'didnr': 2 });
 }
 
 function onCheckBoxClick1() {
 
 	var vc1 = document.getElementById("vc1");
-	browser.storage.local.set({ 'vc1': vc1.checked });
+	api.storage.local.set({ 'vc1': vc1.checked });
 }
 
 function onCheckboxClick2() {
 
 	var vc2 = document.getElementById("vc2");
-	browser.storage.local.set({ 'vc2': vc2.checked });
+	api.storage.local.set({ 'vc2': vc2.checked });
 }
 
 window.addEventListener("load", init, false);
@@ -40,19 +42,19 @@ function init() {
 	vc1.addEventListener("click", onCheckBoxClick1, false);
 	vc2.addEventListener("click", onCheckboxClick2, false);
 
-	browser.storage.local.get('didnr').then(function(value) {
+	api.storage.local.get('didnr').then(function(value) {
 
 		if (value.didnr == 0) did1.checked = true; else did1.checked = false;
 		if (value.didnr == 1) did2.checked = true; else did2.checked = false;
 		if (value.didnr == 2) did3.checked = true; else did3.checked = false;
 	});
 
-	browser.storage.local.get('vc1').then(function(value) {
+	api.storage.local.get('vc1').then(function(value) {
 
 		if (value.vc1 === true) vc1.checked = true; else vc2.checked = false;
 	});
 
-	browser.storage.local.get('vc2').then(function(value) {
+	api.storage.local.get('vc2').then(function(value) {
 
 		if (value.vc2 === true) vc2.checked = true; else vc2.checked = false;
 	});
